4d593cf8add3fab93394ceb0341784caa72b1867
hood <hoods.tech@gmail.com>
2023-07-18 07:27:41 +0300

    Starter

---
+++ app/src/main/java/hoods/com/jetpetrescue/MainActivity.kt
@@ -0,0 +1,34 @@
+package hoods.com.jetpetrescue
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import hoods.com.jetpetrescue.ui.theme.JetPetTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetPetTheme {
+                Greeting("Jetpack Compose")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetPetTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/hoods/com/jetpetrescue/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package hoods.com.jetpetrescue.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val blueBG = Color(0xFFF4F7FD)
+val blueBGNight = Color(0xFF0C1B3A)
+val blueText = Color(0xFF1E3054)
+val pinkText = Color(0xFFF5CAC9)
+val surface = Color(0xFFFFFFFF)
+val surfaceNight = Color(0xFF162544)
